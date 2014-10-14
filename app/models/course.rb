d96fae8596ea031222ee4c3c8b91fdd031664cfb
raielin@gmail.com <raielin@gmail.com>
2014-10-14 18:39:28 -0400

    Course and Spell model files and classes were misnamed - corrected that. Added note in Houses index view for 'No houses found' if no houses. Changed title in Application layout.

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,4 @@
-class Courses < ActiveRecord::Base
+class Course < ActiveRecord::Base
   has_many :spells
   has_many :students, :through => :spells
 end
--- app/models/spell.rb
+++ app/models/spell.rb
@@ -1,4 +1,4 @@
-class Spells < ActiveRecord::Base
+class Spell < ActiveRecord::Base
   belongs_to :student
   belongs_to :course
 end
