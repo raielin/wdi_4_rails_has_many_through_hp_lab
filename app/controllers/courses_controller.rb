3ef12ab039810b455b3ccd070dcd9e1ad93f2a12
raielin@gmail.com <raielin@gmail.com>
2014-10-14 08:12:20 -0400

    Completed Courses controller with all other actions

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -5,6 +5,37 @@ class CoursesController < ApplicationController
     @courses = Course.all
   end
 
+  def show
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def edit
+  end
+
+  def create
+      @course = Course.new(course_params)
+    if @course.save
+      redirect_to @course, notice: 'New course successfully added.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @course.update_attributes(course_params)
+      redirect_to @course, notice: 'Course info sucessfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @course.destroy
+    redirect_to courses_path, notice: "Course was successfully removed."
+  end
 
     private
     def find_course
